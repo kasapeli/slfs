153b5b9051d2eed31a5893c16bdc94dad2ac6d02
kasapeli <yanratanaksambath@gmail.com>
2026-09-20 20:53:13 +0700

    zsh.nix: init

---
+++ desktop/apps/zsh.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      nbr = "sudo nixos-rebuild switch --flake .";
+    };
+  };
+}
+
